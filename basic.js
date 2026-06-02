397c83323df48cad5e55101c80251f68992011be
Ankit-nZ635 <ankiit565@gmail.com>
2026-06-02 20:07:06 +0530

    Delete basic.js

--- basic.js
+++
@@ -1,8 +0,0 @@
-let zone =document.querySelector('h1');
-let bone =document.querySelector('h2');
-let cone =document.querySelector('p');
-
-zone.textContent = "web devlopment";
-bone.textContent = "first class of java script";
-cone.textContent = "i first impementation through js";
-
